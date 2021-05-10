d08ce794279bddc1d07f8ba31198acd18637bed4
rushi <rushikeshbanarase@gmail.com>
2021-05-10 16:34:22 +0530

    commit by ruu

---
+++ src/test/java/qaclickacademy/mavenproject/Demo.java
@@ -0,0 +1,21 @@
+package qaclickacademy.mavenproject;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	
+	@Test
+	
+	public void demo(){
+		System.out.println("demo");
+	}
+	
+	public void demo1(){
+		System.out.println("demo1");
+	}
+	
+	public void demo2(){
+		System.out.println("demo2");
+	}
+}
